e7610d32d9b8707b27daa74ada5d270d1e46480b
Ines Matos <inesgamer2108@gmail.com>
2026-03-10 16:26:49 +0000

    initial commit

---
+++ extratos.hs
@@ -0,0 +1,17 @@
+data Movimento = Credito Float
+               | Debito Float
+               deriving Show
+
+data Data = D Int Int Int
+          deriving Show
+
+data Extracto = Ext Float [(Data, String, Movimento)]
+              deriving Show
+
+data Extractos = Extractos [Extracto]
+               deriving Show
+
+-- Exercicio 1
+
+extValor :: Extracto -> Float -> [Movimento]
+
